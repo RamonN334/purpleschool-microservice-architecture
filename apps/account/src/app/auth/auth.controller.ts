b8e1510c5200a01fe095744425febb17f5544db9
igor.sitnikov <igor.sitnikov@citronium.com>
2023-03-12 00:06:46 +0300

    Implement rmq connection between api and account
    Add API gateway for handle http requests for auth users.
    API gateway recieve http request with register or login data and send
    these data to account microservice through RabbitMQ.
    Add JWT Strategy to generate access_token for user login.

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,24 +1,26 @@
 import { Body, Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AccountLogin, AccountRegister } from '@purpleschool-mono/contracts';
-import { RMQRoute } from 'nestjs-rmq';
+import { RMQRoute, RMQValidate } from 'nestjs-rmq';
 
 @Controller()
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @RMQRoute(AccountRegister.topic)
-    async register(
-        @Body() dto: AccountRegister.Request
-    ): Promise<AccountRegister.Response> {
-        return this.authService.register(dto);
-    }
+  @RMQValidate()
+  @RMQRoute(AccountRegister.topic)
+  async register(
+    @Body() dto: AccountRegister.Request
+  ): Promise<AccountRegister.Response> {
+    return this.authService.register(dto);
+  }
 
-    @RMQRoute(AccountLogin.topic)
-    async login(
-        @Body() { email, password }: AccountLogin.Request
-    ): Promise<AccountLogin.Response> {
-        const { id } = await this.authService.validateUser(email, password);
-        return this.authService.login(id);
-    }
+  @RMQValidate()
+  @RMQRoute(AccountLogin.topic)
+  async login(
+    @Body() { email, password }: AccountLogin.Request
+  ): Promise<AccountLogin.Response> {
+    const { id } = await this.authService.validateUser(email, password);
+    return this.authService.login(id);
+  }
 }
---
+++ apps/api/src/app/strategies/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common/decorators';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { IJWTPayload } from '@purpleschool-mono/interfaces';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
+      ignoreExpiration: true,
+      secretOrkey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate({ id }: IJWTPayload) {
+    return id;
+  }
+}
